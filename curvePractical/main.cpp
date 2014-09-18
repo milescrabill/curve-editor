d50d1c2759fe2474c05ab58148c8f6253d528a82
Miles Crabill <miles@milescrabill.com>
2014-09-18 11:30:19 +0200

    Initial Commit

---
+++ curvePractical/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  curvePractical
+//
+//  Created by Miles Crabill on 9/18/14.
+//  Copyright (c) 2014 Miles Crabill. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
